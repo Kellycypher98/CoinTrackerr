2401e519d0179bb978d05bac63ae2dd160121a39
Kellycypher98 <kelvinboateng94@outlook.com>
2023-08-24 20:50:45 +0000

    fixed import paths

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./../Pages/Dashboard";
-import Transaction from "./../pages/Transaction";
+import Transaction from "./../Pages/Transaction";
 import Account from "./../pages/Account";
 import Report from "./../pages/Report";
 import Budget from "./../pages/Budget";
